fcb67b20c3e359ff66442f177c8499fdad9b2a65
Dann-13 <dannyelgomezz.13@gmail.com>
2023-03-23 17:32:33 -0500

    Subiendo Proyecto: estructura

---
+++ public/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Mi aplicación PHP</title>
+</head>
+<body>
+	<h1>Bienvenido a mi aplicación PHP</h1>
+	<p>Esta es una página de prueba.</p>
+	<p>Funcionando</p>
+	<a href="/Hotel_App/app/views/login_register.php">login</a>
+
+	<?php
+		// Esto es un ejemplo de código PHP
+		$nombre = "Juan";
+		$apellido = "Pérez";
+		echo "<p>Mi nombre es $nombre $apellido</p>";
+	?>
+</body>
+</html>
