e1ebca46c87b966620ae2ce34f8ef1c0fffb14cb
Dann-13 <dannyelgomezz.13@gmail.com>
2023-04-11 22:00:00 -0500

    Pagina Admin, usuarios

---
+++ app/views/admin.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../public/css/admin.css">
+
+    <title>Administrador</title>
+
+</head>
+
+<body>
+    <header>
+        <div class="encabezado">
+            <div class="imgClass">
+                <img src="../../public/img/hotel.png" alt="">
+            </div>
+            <a href="" class="btn__User">Usuarios</a>
+            <a href="" class="btn__reserva">Reservas</a>
+        </div>
+    </header>
+    <main>
+        <h2>Bienvenido! Administrador1</h2>
+        <div class="contenido">
+            <div class="imgClass">
+                <img src="../../public/img/admin.png" alt="" srcset="">
+            </div>
+            <div class="tabla">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Nombre</th>
+                            <th>Correo electrónico</th>
+                            <th>Contraseña</th>
+                            <th colspan="2">Opciones</th> <!-- Columna unida que contiene dos columnas más -->
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>John Doe</td>
+                            <td>john.doe@example.com</td>
+                            <td>********</td>
+                            <td><button class="btn__edit">Editar</button></td>
+                            <td><button class="btn__delete">Eliminar</button></td>
+                        </tr>
+                        <!-- Más filas de datos -->
+                    </tbody>
+                </table>
+
+            </div>
+        </div>
+        <main>
+</body>
+
+</html>
