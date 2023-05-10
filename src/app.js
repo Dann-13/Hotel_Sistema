7f01cde1b5541e1196a8ddeb751053d4c7ae65a4
Dann-13 <dannyelgomezz.13@gmail.com>
2023-05-09 20:52:09 -0500

    Separando rutas de archivo app

--- src/app.js
+++ src/app.js
@@ -13,65 +13,9 @@ app.use('/resources', express.static(__dirname + '/public'))
 app.set('view engine', 'ejs');
 const path = require('path');
 app.set('views', path.join(__dirname, 'views'));
-
-
-//para encriptar contraseñas 
-const bcryptjs = require('bcryptjs');
-
-//variables de session 
-const session = require('express-session');
-app.use(session({
-    secret: 'secret',
-    resave: true,
-    saveUninitialized: true
-}));
-
-//------Modulo de Conexion
-//const connection = require('./data/db')
-const Database = require('./models/database');
-//Inicializamos a base de datos 
-const db = new Database();
-db.connect();
 //----rutas
-app.get('/', (req, res) => {
-    res.render("index")
-})
-app.get('/login_register', (req, res) => {
-    res.render("login_register")
-})
-//registro usuario
-app.post('/login_register', async (req, res) => {
-    const correo = req.body.correo;
-    const contrasena = req.body.contrasena;
-    const nombre = req.body.nombre;
-    const rol = req.body.rol;
-    let passwordHash = await bcryptjs.hash(contrasena, 8);
-    // Insertar el nuevo usuario en la base de datos
-    try {
-        const sql = 'INSERT INTO usuarios (correo, contrasena, nombre, rol) VALUES (?, ?, ?, ?)';
-        const values = [correo, passwordHash, nombre, rol];
-        await db.query(sql, values);
-        res.render('login_register', {
-            alert: true,
-            alertTitle: "Registro",
-            alertMessage: "Registro Exitoso",
-            alertIcon: 'success',
-            showConfirmButton: false,
-            timer: 1500,
-            ruta: ''
-        })
-    } catch (error) {
-        res.render('login_register', {
-            alert: true,
-            alertTitle: "Error",
-            alertMessage: "Incorrecto",
-            alertIcon: "error",
-            showConfirmButton: true,
-            timer: false,
-            ruta: ''
-        });
-    }
-})
+app.use('/', require('./routes/index'));
+//servidor
 app.listen(5000, () => {
     console.log("servidor en http://localhost:5000")
 })
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,53 @@
 const express = require('express');
 const router = express.Router();
+const bcryptjs = require('bcryptjs');
 
+//------Modulo de Conexion
+//const connection = require('./data/db')
+const Database = require('../models/database');
+//Inicializamos a base de datos 
+const db = new Database();
+db.connect();
+//va las rutas
 router.get('/', (req, res) => {
-    res.render('hols');
+    res.render("index")
+})
+router.get('/login_register', (req, res) => {
+    res.render("login_register")
+})
+
+router.post('/login_register', async (req, res) => {
+    const correo = req.body.correo;
+    const contrasena = req.body.contrasena;
+    const nombre = req.body.nombre;
+    const rol = req.body.rol;
+    let passwordHash = await bcryptjs.hash(contrasena, 8);
+    // Insertar el nuevo usuario en la base de datos
+    try {
+        const sql = 'INSERT INTO usuarios (correo, contrasena, nombre, rol) VALUES (?, ?, ?, ?)';
+        const values = [correo, passwordHash, nombre, rol];
+        await db.query(sql, values);
+        res.render('login_register', {
+            alert: true,
+            alertTitle: "Registro",
+            alertMessage: "Registro Exitoso",
+            alertIcon: 'success',
+            showConfirmButton: false,
+            timer: 1500,
+            ruta: ''
+        })
+    } catch (error) {
+        console.log(error)
+        res.render('login_register', {
+            alert: true,
+            alertTitle: "Error",
+            alertMessage: "Incorrecto",
+            alertIcon: "error",
+            showConfirmButton: true,
+            timer: false,
+            ruta: ''
+        });
+    }
 });
 
 module.exports = router;
