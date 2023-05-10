da177c7807fc002c765932a1a041fb08af9a5c7c
Dann-13 <dannyelgomezz.13@gmail.com>
2023-05-09 20:17:28 -0500

    Conexio y registro de usuarios

--- src/app.js
+++ src/app.js
@@ -2,12 +2,8 @@
 const express = require('express');
 const app = express();
 //url encoded para que  no de error en el envio de formularios
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json())
-
-//invocacion y variables de entorno configuracion 
-const dotenv =require('dotenv');
-dotenv.config({path:"./env/.env"})
 
 //directorio public y confguracion para funcionamiento en otras pc
 app.use('/resources', express.static('public'));
@@ -25,23 +21,57 @@ const bcryptjs = require('bcryptjs');
 //variables de session 
 const session = require('express-session');
 app.use(session({
-    secret:'secret',
+    secret: 'secret',
     resave: true,
     saveUninitialized: true
 }));
 
 //------Modulo de Conexion
 //const connection = require('./data/db')
-const DataBase = require('./models/database');
+const Database = require('./models/database');
 //Inicializamos a base de datos 
-DataBase.connect();
+const db = new Database();
+db.connect();
 //----rutas
-app.get('/', (req, res) =>{
+app.get('/', (req, res) => {
     res.render("index")
 })
-app.get('/login', (req, res) =>{
+app.get('/login_register', (req, res) => {
     res.render("login_register")
 })
-app.listen(5000, ()=>{
+//registro usuario
+app.post('/login_register', async (req, res) => {
+    const correo = req.body.correo;
+    const contrasena = req.body.contrasena;
+    const nombre = req.body.nombre;
+    const rol = req.body.rol;
+    let passwordHash = await bcryptjs.hash(contrasena, 8);
+    // Insertar el nuevo usuario en la base de datos
+    try {
+        const sql = 'INSERT INTO usuarios (correo, contrasena, nombre, rol) VALUES (?, ?, ?, ?)';
+        const values = [correo, passwordHash, nombre, rol];
+        await db.query(sql, values);
+        res.render('login_register', {
+            alert: true,
+            alertTitle: "Registro",
+            alertMessage: "Registro Exitoso",
+            alertIcon: 'success',
+            showConfirmButton: false,
+            timer: 1500,
+            ruta: ''
+        })
+    } catch (error) {
+        res.render('login_register', {
+            alert: true,
+            alertTitle: "Error",
+            alertMessage: "Incorrecto",
+            alertIcon: "error",
+            showConfirmButton: true,
+            timer: false,
+            ruta: ''
+        });
+    }
+})
+app.listen(5000, () => {
     console.log("servidor en http://localhost:5000")
 })
--- src/models/database.js
+++ src/models/database.js
@@ -1,21 +1,46 @@
-const mysql = require('mysql');
+const mysql = require('mysql2/promise');
+//invocacion y variables de entorno configuracion 
+require('dotenv').config({ path: __dirname + '/../env/.env' });
 class Database {
-    constructor() {
-        this.connection = mysql.createConnection({
-            host: process.env.DB_HOST,
-            user: process.env.DB_USER,
-            password: process.env.DB_PASSWORD,
-            database: process.env.DB_DATABASE
-        })
+  constructor() {
+    this.connection = null;
+  }
+
+  async connect() {
+    console.log(process.env.DB_HOST)
+    try {
+      this.connection = await mysql.createConnection({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE
+      });
+      await this.connection.query(`USE hotel`);
+
+      console.log('Conexión a la base de datos establecida');
+    } catch (error) {
+      console.error(`Error al conectarse a la base de datos: ${error}`);
     }
-    connect(){
-        this.connection.connect((err) => {
-            if(err){
-                console.log(err)
-                return;
-            }
-            console.log("Conexion exitosa")
-        })
+  }
+
+  async query(sql, values) {
+    try {
+      const [results, fields] = await this.connection.execute(sql, values);
+      return results;
+    } catch (error) {
+      console.error(`Error al ejecutar la consulta: ${error}`);
+      return null;
     }
+  }
+
+  async disconnect() {
+    try {
+      await this.connection.end();
+      console.log('Conexión a la base de datos cerrada');
+    } catch (error) {
+      console.error(`Error al cerrar la conexión a la base de datos: ${error}`);
+    }
+  }
 }
-module.exports = new Database();
+
+module.exports = Database;
